0c08d4e7a65ea8f07e6894d37b93b6299487bb60
Doris Liu <chiayuu.liu@gmail.com>
2022-04-12 18:02:49 +0800

    react routing course

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import Header from "./component/Header";
+import Nav from "./component/Nav";
+import Home from "./component/Home";
+import NewPost from "./component/NewPost";
+import PostPage from "./component/PostPage";
+import About from "./component/About";
+import Missing from "./component/Missing";
+import Footer from "./component/Footer";
+
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+
+      <Switch>
+        <Route exact path="/">
+          <Home posts={posts}/>
+        </Route>
+
+        <Route exact path="/post">
+          <NewPost />
+        </Route>
+
+        <Route path="/post/:id">
+          <PostPage />
+        </Route>
+
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+
+      <Footer />
+    </div>
+  );
+};
+export default App;
